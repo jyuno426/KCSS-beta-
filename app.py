e4519fde9fb396cb792423cb02e4aedf8f689aa8
Junho <jyuno426@kaist.ac.kr>
2018-11-02 13:39:21 +0900

    .

--- app.py
+++ app.py
@@ -2,22 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 30 17:45:03 2018
-
-@author: AB-type1
+@author: Seunghyun Lee, Junho Han
 """
-# import os, sys
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from Name_model import keras_Model
-import tensorflow as tf
-from utils import normalize
-import json
-import numpy as np
-import collections
 from datetime import datetime
-
-
-
-
+from utils import normalize
+import tensorflow as tf
+import numpy as np
+import json
 
 #############################
 #####  HELPER FUNCTIONS #####
@@ -62,7 +55,6 @@ def is_kr_first_name(first_name):
 
     return (0 in idx) and (1 not in idx)
 
-
 def is_kr_last_name(last_name):
     return last_name.lower() in kr_last_names
 
@@ -76,7 +68,6 @@ def init():
     global graph
     graph = tf.get_default_graph()
 
-
 app = Flask(__name__) # placeholder for current module
 
 @app.route('/')
@@ -84,11 +75,8 @@ def home():
     currentYear = datetime.now().year
     return render_template('home.html', years = [x for x in range(2008,currentYear+1)])
 
-
-@app.route('/')
 @app.route('/<name>')
 def display(name):
-    
     fromyear = int(name[0:4])
     toyear = int(name[4:8])
     option = int(name[8])
@@ -108,7 +96,7 @@ def display(name):
                     temp = "_by_last.json"
                 else:
                     temp = "_by_every.json"
-                with open("./dblp/"+journal.upper()+"/"+journal.lower()+str(x)+ temp,'r') as f:
+                with open("./dblp/"+journal.upper()+"/"+journal.lower()+str(x)+temp,'r') as f:
                     dictionary = json.load(f)
                     temp = list(dictionary.keys())
                     for author in temp:
@@ -133,9 +121,6 @@ def display(name):
         temp = author.rfind(" ")
         last = author[temp+1:].lower()
         first = author[:temp].lower()
-        #first = ''
-        #for part in first_raw.split():
-        #    first += normalize(part)
         
         if kroption == 0:
             if last in kr_last_names:
@@ -144,10 +129,6 @@ def display(name):
 
         else:
             if last in kr_last_names:
-                #with graph.as_default():
-                #    prediction = model.pred(first)
-                #if np.argmax(prediction) == 0:
-                #    korean_names.append(author)
                 if is_kr_first_name(first):
                     korean_names.append(author)
                 else:
@@ -157,10 +138,7 @@ def display(name):
 
     korean_names.sort()
     non_korean_names.sort()
-    
-    # korean_names = korean_names[:100]
-    # non_korean_names = non_korean_names[:100]
-    
+
     # Display options:
     # 0. Default: Alphabetical
     # 1. Number of papers
@@ -184,6 +162,9 @@ def display(name):
         non_korean_names = []
         for i in range(len(nonkr)):
             non_korean_names.append(nonkr[i][1])
+
+    korean_names = korean_names[:100]
+    non_korean_names = non_korean_names[:100]
     
     return render_template('display.html',
                            name = str(name).upper(), 
@@ -194,39 +175,9 @@ def display(name):
                            kroption = kroption)
 
 
-
-'''
-def is_kr_first_name(first_name):
-    first = ''
-    idx = set()
-
-    for part in first_name.split():
-        _part = normalize(part)
-        if len(_part) > 1:
-            idx.add(np.argmax(predict(_part)))
-            first += _part
-    if len(first) > 1:
-        arg = np.argmax(predict(first))
-        if arg == 0: return True
-        idx.add(arg)
-
-    return (0 in idx) and (1 not in idx)
-
-
-def predict(name):
-    with graph.as_default():
-        prediction = model.pred(name)
-    return prediction
-'''
-
-
-
-
-
 if __name__ == '__main__':
-    
     kr_last_names = set()
     model = keras_Model()
     graph = None
     init()
-    app.run(port = 5003)
+    app.run(port = 5002)
