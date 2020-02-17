6253ea22cd5fa1cec6adff1de2a428e5a0d4a2fa
Junho <jyuno426@kaist.ac.kr>
2020-02-17 18:58:51 +0900

    kr_hard_coding/last name Mo

--- db_maker.py
+++ db_maker.py
@@ -287,7 +287,8 @@ if __name__ == '__main__':
     # db_maker.make_conf_db('interspeech', min_year, max_year)
 
     # db_maker.make_conf_year_db('sigmetrics', 2018)
-    # db_maker.fix_db(min_year, max_year)
+    db_maker.fix_db(min_year, max_year)
     # print(db_maker.is_kr('Sungjin Im'))
     # db_maker.make_configuration(min_year, max_year)
     # db_maker.make_all_db(min_year, max_year)
+    # print(db_maker.prob_kr('Sangwoo Mo'))
